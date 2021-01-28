32e0b513090311b274c1ddbd991c245afe4fb80b
Anicet Muzaliwa <anicetmuzas@gmail.com>
2021-01-28 07:28:26 +0100

    ajouter du texte et la modification de la couleur

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>lifti stage</h2>
+      <p>lorem kdlklqklfkslfklqlrkflkrlfkml</p>
+    </div>
+
+  );
+}
+
+export default App;
